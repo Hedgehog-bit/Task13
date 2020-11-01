6ca986fc0f1a43040daa1597771a575c0c240cc8
Hedgehog-bit <65031811+Hedgehog-bit@users.noreply.github.com>
2020-11-01 19:00:10 +0300

    Add files via upload

---
+++ Консоль13/Goods.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Задание13
+{
+    public abstract class Goods
+    {
+        protected string _name;
+        protected string _manufacturers;//производитель/издательство
+        protected double _price;
+        protected int _age;
+        protected int _Id;
+        protected Goods(string name, string manufacture, double price, int age)
+        {
+            _name = name;
+            _manufacturers = manufacture;
+            _price = price;
+            _age = age;
+        }
+        public abstract void info();
+        public bool valid(int num)
+        {
+            if (num == _Id) return true;
+            else return false;
+        }
+    }
+    public class Toy : Goods
+    {
+        public Toy(string name, string manufacture, double price, int age) : base(name, manufacture, price, age)
+        {
+            _Id = 1;
+        }
+        public override void info()
+        {
+            Console.WriteLine($"ИГРУШКА\nНазвание: {_name}\nЦена: {_price}\nПроизводитель:{_manufacturers}\nВозраст, на который рассчитана:{_age}\n");
+        }
+    }
+    public class Book : Goods
+    {
+        private string _autor;
+        public Book(string name, string manufacture, string autor, double price, int age) : base(name, manufacture, price, age)
+        {
+            _Id = 2;
+            _autor = autor;
+        }
+        public override void info()
+        {
+            Console.WriteLine($"КНИГА\nНазвание: {_name}\nЦена: {_price}\nАвтор: {_autor}\nИздатель:{_manufacturers}\nВозраст, на который рассчитана:{_age}\n");
+        }
+    }
+    public class Sport : Goods
+    {
+        public Sport(string name, string manufacture, double price, int age) : base(name, manufacture, price, age)
+        {
+            _Id = 3;
+        }
+        public override void info()
+        {
+            Console.WriteLine($"СПОРТ-ТОВАР\nНазвание: {_name}\nЦена: {_price}\nПроизводитель:{_manufacturers}\nВозраст, на который рассчитана:{_age}\n");
+        }
+    }
+}
---
+++ Консоль13/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Задание13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string path = @"C:\Users\\egora\Desktop\tovar.txt"; ;
+                List<Goods> collection = new List<Goods>();
+                string[] all_lines = File.ReadAllLines(path);
+                for (int i = 0; i < all_lines.Length; i++)
+                {
+                    string[] current_position = all_lines[i].Split(';');
+                    if (Convert.ToInt32(current_position[0]) == 1)
+                    {
+                        Toy toy = new Toy(current_position[1], current_position[2], Convert.ToDouble(current_position[3]), Convert.ToInt32(current_position[4]));
+                        collection.Add(toy);
+                    }
+                    if (Convert.ToInt32(current_position[0]) == 2)
+                    {
+                        Book book = new Book(current_position[1], current_position[2], current_position[3], Convert.ToDouble(current_position[4]), Convert.ToInt32(current_position[5]));
+                        collection.Add(book);
+                    }
+                    if (Convert.ToInt32(current_position[0]) == 3)
+                    {
+                        Sport sport = new Sport(current_position[1], current_position[2], Convert.ToDouble(current_position[3]), Convert.ToInt32(current_position[4]));
+                        collection.Add(sport);
+                    }
+                }
+                foreach (Goods var in collection)
+                    var.info();
+                Console.Write("Введите номер категории (1 - игрушка, 2 - книга, 3 - спорт-товар): "); int des = Convert.ToInt32(Console.ReadLine());
+                if (des > 3 && des < 1) throw new Exception("Некорректный ввод");
+                List<Goods> search = new List<Goods>();
+                foreach (Goods var in collection)
+                {
+                    if (var.valid(des)) search.Add(var);
+                }
+                Console.WriteLine("Результат поиска по категории: ");
+                foreach (Goods var in search)
+                    var.info();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
